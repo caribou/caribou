adea9fa4f80ba69fb7263ff2cfa0926388b024ac
ryan spangler <ryan.spangler@gmail.com>
2011-07-25 11:47:39 -0700

    model lifecycle testing

--- test/triface/test/model.clj
+++ test/triface/test/model.clj
@@ -3,20 +3,26 @@
   (:use [triface.model])
   (:use [clojure.test]))
 
-(deftest create-model-test
+(deftest model-lifecycle-test
   (let [model (create-model {:name "yellow"
                              :description "yellowness yellow yellow"
                              :position 3
                              :fields [{:name "gogon" :type "string"}
-                                      {:name "wibib" :type "boolean"}]})]
+                                      {:name "wibib" :type "boolean"}]})
+        yellow (db/insert :yellow {:gogon "obobo" :wibib true})]
 
     (is (>= 8 (count (model :fields))))
     (is (= (model :name) "yellow"))
     (is ((models :yellow) :name "yellow"))
     (is (db/table? "yellow"))
-
+    (is (yellow :wibib))
+    (is (= 1 (count (db/query "select * from yellow"))))
+    
     (delete-model "yellow")
 
     (is (not (db/table? "yellow")))
     (is (not (models :yellow)))))
+
+
+
 
