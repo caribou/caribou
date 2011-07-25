ccc8d99812bb2441cdc2a939887f295107971878
eric feigner <eric@weareinstrument.com>
2011-07-25 11:25:52 -0700

    reworking content-list-test to mock db/query results

--- test/triface/test/core.clj
+++ test/triface/test/core.clj
@@ -1,8 +1,9 @@
 (ns triface.test.core
   (:use [triface.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:require [triface.db :as db]))
 
-(deftest core-test
-  (is true (> (count (content-list "model")) 0)))
-
-
+;; simple content-list test, mocking the underlying db/query call to return an expected result
+(deftest content-list-test
+  (binding [db/query (fn [query & args] (array-map :id 1 :name "model"))]
+    (is (> (count (content-list "model")) 0))))
