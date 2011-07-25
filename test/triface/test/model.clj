e4b48a69cbda4a7a2672568ced8bebc780f54b06
ryan spangler <ryan.spangler@gmail.com>
2011-07-25 00:51:42 -0700

    create model and delete model working, with tests

---
+++ test/triface/test/model.clj
@@ -0,0 +1,18 @@
+(ns triface.test.model
+  (:require [triface.db :as db])
+  (:use [triface.model])
+  (:use [clojure.test]))
+
+(deftest create-model-test
+  (is (= 8 (count ((create-model {:name "yellow"
+                                  :description "yellowness yellow yellow"
+                                  :position 3
+                                  :fields [{:name "gogon" :type "string"}
+                                           {:name "wibib" :type "boolean"}]}) :fields))))
+  (is ((models :yellow) :name "yellow"))
+  (is (db/table? "yellow"))
+
+  (delete-model "yellow")
+
+  (is (not (db/table? :yellow)))
+  (is (not (models :yellow))))
