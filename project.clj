bb49abcc78d9c803573d3f73f20402a3cf431b30
ryan spangler <ryan.spangler@gmail.com>
2011-09-14 16:34:52 -0700

    working on testing nested structures

--- project.clj
+++ project.clj
@@ -7,14 +7,16 @@
                  [compojure "0.6.4"]
                  [clojure-solr "0.2.0"]
                  [ring/ring-jetty-adapter "0.3.10"]
-                 [com.instrument/triface-jython-object-factory "1.0-SNAPSHOT"]]
+                 ;;[com.instrument/triface-jython-object-factory "1.0-SNAPSHOT"]
+                 ]
   :dev-dependencies [[lein-ring "0.4.5"]
                      [org.clojure/java.jdbc "0.0.6"]
                      [postgresql/postgresql "8.4-702.jdbc4"]
                      [swank-clojure "1.4.0-SNAPSHOT"]
                      [clojure-source "1.2.0"]
                      [lein-eclipse "1.0.0"]
-                     [com.instrument.triface/triface-action-adapter "1.0-SNAPSHOT"]]
+                     ;;[com.instrument.triface/triface-action-adapter "1.0-SNAPSHOT"]
+                     ]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :ring {:handler triface.core/app
          :servlet-name "triface"
