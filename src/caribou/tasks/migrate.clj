2bafb1f92710237f3f8bcc8cfa0028826909d337
ryan spangler <ryan.spangler@gmail.com>
2013-08-15 10:50:50 -0700

    moved everything out to separate repos

--- src/caribou/tasks/migrate.clj
+++
@@ -1,8 +0,0 @@
-(ns leiningen.migrate
-  (:require [caribou.migration :as mm]))
-
-(defn migrate [name]
-  (mm/run-migrations name))
-
-(defn -main [name]
-  (migrate name))
--- test/caribou/test/util.clj
+++
@@ -1,9 +0,0 @@
-(ns caribou.test.util
-  (:require [caribou.util :as util])
-  (:use [clojure.test]))
-
-(import java.io.File)
-
-(deftest get-file-extension-test
-  (let [file (File. "/foo/bar/baz/ack/bar/froot.rb")]
-    (is (= ".rb" (util/get-file-extension file)))))
