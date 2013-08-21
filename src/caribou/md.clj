fff9e42a4492f242d87e46b5716f437397e42b60
ryan spangler <ryan.spangler@gmail.com>
2013-08-21 15:01:00 -0700

    wrapped pages with layout

--- src/caribou/md.clj
+++ src/caribou/md.clj
@@ -19,10 +19,50 @@
    "api"
    "deploying"])
 
+(def pre-layout
+  "<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv=\"X-UA-Compatible\" content=\"chrome=1\">
+
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/stylesheet.css\" media=\"screen\" />
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/pygment_trac.css\" media=\"screen\" />
+
+    <title>Caribou Documentation</title>
+  </head>
+
+  <body>
+
+    <header>
+      <div class=\"container\">
+        <h1>THIS. IS. CARIBOU.</h1>
+      </div>
+    </header>
+
+    <div class=\"container\">
+      <section id=\"main_content\">
+")
+
+(def post-layout
+  "
+      </section>
+    </div>
+
+    
+  </body>
+</html>")
+
+(defn wrap-layout
+  [html]
+  (str pre-layout html post-layout))
+
 (defn compile-targets
   []
   (doseq [target targets]
-    (md/md-to-html (str target ".md") (str "docs/" target ".html"))))
+    (let [html (md/md-to-html-string (slurp (str target ".md")))
+          wrapped (wrap-layout html)]
+      (spit (str "docs/" target ".html") wrapped))))
 
 (defn -main
   []
